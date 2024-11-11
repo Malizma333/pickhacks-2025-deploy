f2c64f3a5868c658000e3be1c96a92d4cc797bcb
tkbessler <tkbessler@gmail.com>
2024-11-11 13:54:58 -0600

    forward and back buttons

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -7,6 +7,8 @@ import PushButton from '@/components/PushButton';
 const Schedule = () => {
 	const [selected, setSelected] = useState<string>('Friday');
 	const [currData, setCurrData] = useState<IEvent[]>([]);
+  const [page, setPage] = useState<number>(0);
+  const eventsPerPage = 4;
 
 	useEffect(() => {
 		setCurrData([...Events.filter((obj) => obj.day === selected)]);
@@ -15,15 +17,21 @@ const Schedule = () => {
 	const toggle = (day: string) => {
 		if (selected != day) {
 			setSelected(day);
+      setPage(0);
 			setCurrData([...Events.filter((obj) => obj.day === day)]);
 		}
 	};
+
+  const forwardPage = () => setPage(page + 1);
+  const backPage = () => setPage(page - 1);
+
 	return (
 		<div className={styles.App}>
       <p className={styles.header}>Schedule</p>
 			<div className={styles.row}>
 				{['Friday', 'Saturday', 'Sunday'].map((day) => (
           <PushButton
+            key={day}
             variant={selected === day ? 'secondary' : 'primary'}
             size='lg'
             onClick={() => toggle(day)}
@@ -31,7 +39,7 @@ const Schedule = () => {
           ></PushButton>
 				))}
 			</div>
-			{currData.map((obj) => (
+			{currData.slice(page * eventsPerPage, (page + 1) * eventsPerPage).map((obj) => (
 				<div key={`${obj.day}-${obj.event}-${obj.time}`} className={styles.eventContainer}>
 					<div className={styles.column}>
 						<p className={styles.event}>{obj.event}</p>
@@ -42,6 +50,18 @@ const Schedule = () => {
 					</div>
 				</div>
 			))}
+      <button
+        className={styles.pageNavButton}
+        style={{ right: 0 }}
+        disabled={(page + 1) * eventsPerPage >= currData.length}
+        onClick={forwardPage}
+      >{">"}</button>
+      <button
+        className={styles.pageNavButton}
+        style={{ left: 0 }}
+        disabled={page === 0}
+        onClick={backPage}
+      >{"<"}</button>
 		</div>
 	);
 };
