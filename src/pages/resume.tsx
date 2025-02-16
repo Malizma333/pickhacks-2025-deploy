fc0c337f0feb261fdbdbecfd757b5e1c75da9939
Matthew <68716884+2406mmartin@users.noreply.github.com>
2025-02-16 11:49:30 -0600

    Redo resume form

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -8,10 +8,14 @@ const ResumeSubmission = () => {
 	const [email, setEmail] = useState('');
 	const [loading, setLoading] = useState(false);
 	const [message, setMessage] = useState('');
+	const [fileStatus, setFileStatus] = useState('NO FILE UPLOADED');
 
 	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		if (event.target.files) {
 			setResume(event.target.files[0]);
+			setFileStatus("FILE UPLOADED")
+		} else {
+			setFileStatus("NO FILE UPLOADED")
 		}
 	};
 
@@ -55,21 +59,29 @@ const ResumeSubmission = () => {
 			<form onSubmit={handleSubmit} className={styles.form}>
 				<div>
 					<label htmlFor='firstName'>First Name</label>
-					<input type='text' id='firstName' value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
+					<input className={styles.input} type='text' id='firstName' value={firstName} onChange={(e) => setFirstName(e.target.value)} required />
 				</div>
 				<div>
 					<label htmlFor='lastName'>Last Name</label>
-					<input type='text' id='lastName' value={lastName} onChange={(e) => setLastName(e.target.value)} required />
+					<input className={styles.input} type='text' id='lastName' value={lastName} onChange={(e) => setLastName(e.target.value)} required />
 				</div>
 				<div>
 					<label htmlFor='email'>Email</label>
-					<input type='email' id='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
+					<input className={styles.input} type='email' id='email' value={email} onChange={(e) => setEmail(e.target.value)} required />
 				</div>
 				<div>
-					<label htmlFor='resume'>Upload Resume</label>
-					<input type='file' accept='.pdf,.doc,.docx' onChange={handleFileChange} required style={{color: '#C3C6E2'}}/>
+					<label htmlFor="resume">Upload Resume</label>
+					<div className={styles.uploadFile}>
+						<label className={styles.customFileLabel}>
+							<svg className={styles.customFileLabelIcon} fill="none" xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24">
+								<path d="M11 5V3h2v2h2v2h2v2h-2V7h-2v10h-2V7H9v2H7V7h2V5h2zM3 15v6h18v-6h-2v4H5v-4H3z" fill="currentColor" />
+							</svg>
+							<input className={styles.hiddenFileInput} type="file" accept=".pdf,.doc,.docx" onChange={handleFileChange} required />
+						</label>
+						{fileStatus && <label id="file-status">{fileStatus}</label>}
+					</div>
 				</div>
-				<button type='submit' disabled={loading}>
+				<button className={styles.submit} type='submit' disabled={loading}>
 					{loading ? 'Submitting...' : 'Submit Resume'}
 				</button>
 			</form>
