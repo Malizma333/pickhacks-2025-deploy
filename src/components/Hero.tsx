9e3c2a52146754358c7546d15caf6d2ef28a04ff
Malizma333 <malizma333@gmail.com>
2025-09-05 16:12:13 -0500

    update home

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,9 +12,14 @@ const Hero = () => {
         <PushButton
           variant="primary"
           width="max(115px, 15vw)"
-          onClick={() => window.open("example.com", "_blank")}
+          onClick={() =>
+            window.open(
+              "https://file.notion.so/f/f/3ccc68c2-cd58-4f1e-b62a-55605f473760/b53fbc14-0350-4249-a170-22acb4786546/Sponsorship_Prospectus_2025_(2).pdf?table=block&id=34bfb8cd-4ee3-43fc-94d4-008c1a103d83&spaceId=3ccc68c2-cd58-4f1e-b62a-55605f473760&expirationTimestamp=1757203200000&signature=sAbup9LjJJZLhLuXji9TbbQ7nUvfiJ0fxwf8vG3UfN0&downloadName=Sponsorship+Prospectus+2025+%282%29.pdf",
+              "_blank"
+            )
+          }
         >
-          Sponsor PickHacks 2026
+          Sponsor 2026
         </PushButton>
       </div>
     </div>
